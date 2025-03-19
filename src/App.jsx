7784bd25ca07e70f69a9f3b12f76b653afac917c
minjaesong04 <minjae.song@mail.utoronto.ca>
2025-03-19 17:59:22 -0400

    bugfixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./components/About"
 import Hero from "./components/Hero"
-import Navbar from "./components/navbar"
+import Navbar from "./components/Navbar"
 import Technologies from "./components/Technologies"
 import Experience from "./components/Experience"
 import Projects from "./components/Projects"
